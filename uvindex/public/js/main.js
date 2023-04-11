18d4d234d177083c6c05d5801708ebcd8573b945
Dlovan <medes_117@live.se>
2023-04-11 11:55:37 +0200

    working on mobile menu and hamburger

--- uvindex/public/js/main.js
+++ uvindex/public/js/main.js
@@ -10,9 +10,36 @@
 //     console.log(error)
 // }
 
+
+// Search results list effect
 const searchBar = document.querySelector('#searchBar')
 const searchResults = document.querySelector('.search-results')
 
 searchBar.addEventListener('input', function () {
     searchResults.classList.add('search-results-input')
+})
+
+
+// Mobile menu
+const hamburger = document.querySelector('.hamburger-menu')
+const mobileMenu = document.querySelector('.mobile-menu')
+const lineOne = document.querySelector('.line1')
+const lineTwo = document.querySelector('.line2')
+const lineThree = document.querySelector('.line3')
+
+let menuStatus = false
+
+hamburger.addEventListener('click', function () {
+    if (menuStatus === false) {
+        menuStatus = true
+        mobileMenu.classList.add('mobile-menu-true')
+        lineOne.classList.add('line1-effect')
+        lineTwo.classList.add('line2-effect')
+        lineThree.classList.add('line3-effect')
+        document.body.classList.add('no-scroll')
+    } else {
+        menuStatus = false
+        mobileMenu.classList.remove('mobile-menu-true')
+        document.body.classList.remove('no-scroll')
+    }
 })
